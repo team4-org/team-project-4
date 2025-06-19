4c37a59957160fd1eaa4bf65208d52d33135a582
Nataliex-Aleks <ns.aleksandrova@mail.ru>
2025-06-19 21:04:01 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,14 +2,12 @@ import tkinter as tk
 from tkinter import messagebox
 
 class TicTacToe:
-    def __init__(self, master):
+    def init(self, master):
         self.master = master
         master.title("Крестики-нолики")
-
         self.board = [" "] * 9
         self.current_player = "X"
         self.buttons = []
-
         for i in range(9):
             button = tk.Button(
                 master,
@@ -24,40 +22,16 @@ class TicTacToe:
 
     def check_win(self, player):
         for i in range(3):
-            if self.board[i*3] == self.board[i*3 + 1] == self.board[i*3 + 2] == player:
+            if self.board[i*3] == self.board[i*3+1] == self.board[i*3+2] == player:
                 return True
         for i in range(3):
-            if self.board[i] == self.board[i + 3] == self.board[i + 6] == player:
+            if self.board[i] == self.board[i+3] == self.board[i+6] == player:
                 return True
         if self.board[0] == self.board[4] == self.board[8] == player:
             return True
         if self.board[2] == self.board[4] == self.board[6] == player:
             return True
-                def check_draw(self):
-        return " " not in self.board
-
-    def button_click(self, index):
-        if self.board[index] == " ":
-            self.board[index] = self.current_player
-            self.buttons[index].config(text=self.current_player)
-
-            if self.check_win(self.current_player):
-                messagebox.showinfo("Игра окончена", f"Игрок {self.current_player} победил!")
-                self.reset_game()
-            elif self.check_draw():
-                messagebox.showinfo("Игра окончена", "Ничья!")
-                self.reset_game()
-            else:
-                self.current_player = "O" if self.current_player == "X" else "X"
-                    def reset_game(self):
-        self.board = [" "] * 9
-        self.current_player = "X"
-        for button in self.buttons:
-            button.config(text=" ")
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    game = TicTacToe(root)
-    root.mainloop()
         return False
+
+    def check_draw(self):
+        return " " not in self.board
